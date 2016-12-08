2879592f97091545c52f59e1c12ff9377464599c
baayl3669 <baayl3669@HRH-IC0027339.wrdsb.ca>
2016-12-08 09:22:49 -0500

    added comments on question 1

--- src/A7Q1.java
+++ src/A7Q1.java
@@ -20,22 +20,24 @@ public class A7Q1 {
         // create scanner 
         Scanner in = new Scanner(System.in);
         
-        //
+        // ask user how many students in the class
         System.out.println("How many students are in your class ?");
         int students = in.nextInt();
         int[] nums = new int[students];
-        //
+        
+        // ask for the marks and put into array
         System.out.println("Enter the marks");
         for(int i = 0; i < nums.length; i++){
             nums[i] = in.nextInt();            
         }
-        //
+        // calculate total of marks
         int total = 0;
         for(int i = 0; i < nums.length; i++){
             total = total + nums[i];
         }
         total = total/students;
-        //
+       
+        // print out total
         System.out.println("The average is " + total);
         
     }
